0e0402467069ac6a3bde14c819570546459832cd
Argeny Tavarez <argenyjtavarez@gmail.com>
2023-04-04 12:59:42 -0400

    Show records in tableviews

--- client/src/TableView.js
+++ client/src/TableView.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { makeStyles } from '@mui/styles';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, cardHeaderClasses } from '@mui/material';
 
 const useStyles = makeStyles({
   table: {
@@ -10,18 +10,48 @@ const useStyles = makeStyles({
 
 function TableView({ view, sheetData }) {
   console.log(sheetData)
+  const [filteredColumns, setFilteredColumns] = useState([])
+  const [datasource, setDatasource] = useState({})
+
+  useEffect(() => {
+    const getFilteredColumns = async () => { 
+      console.log('IN GETFITERED COLUMNS')
+      const allColumns = view.table.columns
+      const shownColumns = view.columns
+      var headers = sheetData.sheet_data[0]
+      console.log(headers)
+      console.log(allColumns)
+      const init_columns = allColumns.filter(column => shownColumns.includes(column._id))
+        .map(obj => ({ ...obj, index: headers.findIndex(header => header === obj.name)}));
+      console.log(init_columns)
+      setFilteredColumns(init_columns)
+      setDatasource(sheetData)
+    };
+    getFilteredColumns();
+  }, [view, sheetData])
+  // useEffect(() => {
+  //   const getSheetData = async () => {
+  //     setDatasource(sheetData)
+  //   };
+  //   getSheetData();
+  // }, [sheetData])
   //console.log(Object.keys(sheetData).length)
   const classes = useStyles();
-
-  const allColumns = view.table.columns
-  const shownColumns = view.columns
-  const filteredColumns = allColumns.filter(column => shownColumns.includes(column._id));
-  console.log(filteredColumns)
+  //const header = sheetData[0]
+  // const allColumns = view.table.columns
+  // const shownColumns = view.columns
+  //const filteredColumns = allColumns.filter(column => shownColumns.includes(column._id));//.map(obj => ({ ...obj, index: header.findIndex(obj.name)}));
+  // console.log(filteredColumns)
 
   const handleClickRecord = (record) => {
 
   }
-
+  if (filteredColumns.length === 0) {
+    return <div>Loading views...</div>;
+  }
+  else{
+    console.log(filteredColumns)
+  }
   return (
     <div>
       <h2>{view.name}</h2>
@@ -36,13 +66,18 @@ function TableView({ view, sheetData }) {
           </TableHead>
           <TableBody>
             {/* Here we populate the rows with actual data */}
-            {/* {typeof sheetData.sheet_data !== 'undefined' &&  (sheetData.sheet_data).map((record) => (
-              <TableRow key={view._id} className={classes.tableRow} onClick={() => handleClickRecord(record)}>
-                {filteredColumns.map((column, index) => (
-                  <TableCell key={column._id}>{record[index] || ''}</TableCell>
-                ))}
-              </TableRow>
-            ))} */}
+            {
+
+            }
+            {typeof sheetData.sheet_data !== 'undefined' &&  (sheetData.sheet_data).slice(1).map((record, index) =>(
+                <TableRow key={index} className={classes.tableRow} onClick={() => handleClickRecord(record)}>
+                  {filteredColumns.map((column) => (
+                    <TableCell key={column._id}>{record[column.index] || ''}</TableCell>
+                  ))} 
+                  </TableRow>
+              )
+            )
+          }
           </TableBody>
         </Table>
       </TableContainer>

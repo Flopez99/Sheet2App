7580b8e0d28203a97e658a6ed1fed37cd55602aa
saminmalik <samin.malik@stonybrook.edu>
2023-04-06 20:53:45 -0400

    implemented a pop up to add new record. *Needs additional logic to add data to the database*

--- client/src/TableView.js
+++ client/src/TableView.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@mui/styles';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Stack, Container, Grid, Button, Box } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions } from '@mui/material';
 
 const useStyles = makeStyles({
   table: {
@@ -24,6 +25,9 @@ function TableView({ view, sheetData, onClickRecord, userEmail }) {
   const [keyIndex, setKeyIndex] = useState(0);
   const [filterIndex, setFilterIndex] = useState(-1);
   const [userFilterIndex, setUserFilterIndex] = useState(-1);
+  const [open, setOpen] = useState(false);
+  const [newRecord, setNewRecord] = useState({});
+
 
   useEffect(() => {
     const getFilteredColumns = async () => {
@@ -88,9 +92,17 @@ function TableView({ view, sheetData, onClickRecord, userEmail }) {
 
   const handleAddRecord = () => {
     console.log('Add Record Button Clicked');
-    // NEED TO CREATE ADD_RECORD COMPONENT 
-    // so that it redirects to a pop up or different page
+    setOpen(true);
   };
+
+  const handleSubmit = () => {
+    console.log('Submit new record:', newRecord);
+    // Logic to be added to push the data to the server
+    // Update the SheetData and re-render in DisplayApp ig
+    setOpen(false);
+    setNewRecord({});
+  };
+  
 
   if (filteredColumns.length === 0) {
     return <div>Loading views...</div>;
@@ -156,124 +168,35 @@ function TableView({ view, sheetData, onClickRecord, userEmail }) {
             </TableContainer>
           </Box>
         </Container>
-      </Typography>
+      </Typography>   
+      {/* POP UP HAPPENING HERE TO ADD DATA FOR NEW RECORD*/}  
+      <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="add-record-dialog">
+        <DialogTitle id="add-record-dialog">Add New Record</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            {filteredColumns.map((column) => (
+              <Grid item xs={12} key={column._id}>
+                <TextField
+                  label={column.name}
+                  fullWidth
+                  value={newRecord[column.index] || ''}
+                  onChange={(e) => setNewRecord({ ...newRecord, [column.index]: e.target.value })}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Stack>
   );  
 }
 
 export default TableView;
-
-
-
-// import React, { useEffect, useState} from 'react';
-// import { makeStyles } from '@mui/styles';
-// import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Stack } from '@mui/material';
-// import { Container } from '@mui/system';
-
-// // const useStyles = makeStyles({
-// //   table: {
-// //     minWidth: 650,
-// //   },
-// // });
-// const useStyles = makeStyles({
-//   table: {
-//     minWidth: 650,
-//   },
-//   header: {
-//     backgroundColor: 'rgba(0, 0, 0, 0.05)',
-//     fontWeight: 'bold',
-//   },
-// });
-
-// function TableView({ view, sheetData, onClickRecord }) {
-//   console.log(sheetData)
-//   const [filteredColumns, setFilteredColumns] = useState([])
-//   const [datasource, setDatasource] = useState({})
-//   const [keyIndex, setKeyIndex] = useState(0)
-
-//   useEffect(() => {
-//     const getFilteredColumns = async () => { 
-//       console.log('IN GETFITERED COLUMNS')
-//       const allColumns = view.table.columns
-//       const shownColumns = view.columns
-//       var headers = sheetData.sheet_data[0]
-//       console.log(headers)
-//       console.log(allColumns)
-//       let key_col = allColumns.find(column => column._id = sheetData.key)
-//       let key_index = headers.findIndex(header => header === key_col.name)
-//       console.log(key_index)
-//       const init_columns = allColumns.filter(column => shownColumns.includes(column._id))
-//         .map(obj => ({ ...obj, index: headers.findIndex(header => header === obj.name)}));
-//       console.log(init_columns)
-//       setKeyIndex(key_index)
-//       setFilteredColumns(init_columns)
-//       setDatasource(sheetData)
-//     };
-//     getFilteredColumns();
-//   }, [view, sheetData])
-//   // useEffect(() => {
-//   //   const getSheetData = async () => {
-//   //     setDatasource(sheetData)
-//   //   };
-//   //   getSheetData();
-//   // }, [sheetData])
-//   //console.log(Object.keys(sheetData).length)
-//   const classes = useStyles();
-//   //const header = sheetData[0]
-//   // const allColumns = view.table.columns
-//   // const shownColumns = view.columns
-//   //const filteredColumns = allColumns.filter(column => shownColumns.includes(column._id));//.map(obj => ({ ...obj, index: header.findIndex(obj.name)}));
-//   // console.log(filteredColumns)
-
-//   const handleClickRecord = (record, other) => {
-//     onClickRecord(record,other)
-//   }
-
-
-//   if (filteredColumns.length === 0) {
-//     return <div>Loading views...</div>;
-//   }
-//   else{
-//     console.log(filteredColumns)
-//   }
-//   return (
-//     <Stack spacing={2} >
-//       <Typography variant="h4" component="h2">
-//         {view.name}
-//       </Typography>
-//       <Typography>
-//       <Container>
-//       <TableContainer component={Paper} elevation = {4} >
-//         <Table className={classes.table} aria-label="simple table">
-//           <TableHead>
-//             <TableRow>
-//               {filteredColumns.map((column) => (
-//                 <TableCell key={column._id} className={classes.header}>
-//                   {column.name}
-//                 </TableCell>
-//               ))}
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {typeof sheetData.sheet_data !== 'undefined' &&
-//               sheetData.sheet_data.slice(1).map((record) => (
-//                 <TableRow
-//                   key={record[keyIndex]}
-//                   className={classes.tableRow}
-//                   onClick={() => handleClickRecord(record, record[keyIndex])}
-//                 >
-//                   {filteredColumns.map((column) => (
-//                     <TableCell key={column._id}>{record[column.index] || ''}</TableCell>
-//                   ))}
-//                 </TableRow>
-//               ))}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//       </Container>
-//       </Typography>
-//     </Stack>
-//   );
-// }
-
-// export default TableView;

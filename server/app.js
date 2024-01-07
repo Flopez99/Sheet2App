d46668aef522ca5d7df39a20aaa1b9ec90814d62
Facundo Lopez <facundo.lopezcamino@stonybrook.edu>
2024-01-07 18:36:50 -0500

    Fixing server

--- server/app.js
+++ server/app.js
@@ -34,13 +34,17 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.get("/", (req, res) => {
 
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+// // Handle React routing, return all requests to React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+// });
 
 // Gets the spreadsheet ID from the role_membership_url
 const getSpreadsheetIdFromUrl = (url) => {
